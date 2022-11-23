10ef1888f69dad87f595d631569836fcacb831fa
Davidb-2107$ git config --listgit config --global user.name Davidb-2107 <david.b2107@gmail.com>
2022-11-23 19:20:23 +0100

    HTML base & JS getting value

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -1,1 +1,14 @@
-//Write your pseduo code first! 
+//Write your pseudo code first! 
+
+// GET the value from the input & save it
+let select = document.querySelector("button");
+
+//ON CLICK, call the function
+select.addEventListener("click", getValue)
+
+function getValue() {
+    let celsiusValue = document.querySelector('input[type="text"]').value
+    // test
+    console.log(celsiusValue)
+}
+

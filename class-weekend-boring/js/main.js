d08d3e52ca2e2160b83046b7919387973f5ed35a
Davidb-2107$ git config --listgit config --global user.name Davidb-2107 <david.b2107@gmail.com>
2022-11-23 19:08:13 +0100

    final JS

--- class-weekend-boring/js/main.js
+++ class-weekend-boring/js/main.js
@@ -1,2 +1,22 @@
 //Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
 
+
+const check = document.querySelector("#check");
+check.addEventListener("click", boring);
+
+function boring() {
+ //GET the value from input
+let inputValue = document.querySelector("#day").value.toLowerCase();
+console.log(inputValue);
+
+if(inputValue === "tuesday" || inputValue === "thursday") {
+    document.querySelector("#placeToSee").textContent = "You have class"
+    
+} else if (inputValue === "saturday" || inputValue === "sunday"){
+        document.querySelector("#placeToSee").textContent = "week-end";
+    } else {
+        document.querySelector("#placeToSee").textContent = "boring";
+    }
+
+};
+
